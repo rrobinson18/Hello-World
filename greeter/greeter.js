e2ac4f4e72317a75fe92a421d29f68b20acdd962
Rakesha McIntyre <robinson.rakesha@gmail.com>
2018-02-06 19:01:58 -0500

    New changes

---
+++ greeter/greeter.js
@@ -0,0 +1,9 @@
+var name = prompt('What is your name?');
+
+var checkName = function(){
+  if(name === "") {
+name = prompt('What is your name, for real this time?');
+  }
+}
+checkName();
+alert('Hello' + name);
